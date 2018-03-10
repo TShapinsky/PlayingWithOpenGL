0656b9f40e553a157074ee27ca57f5a7d99fe688
Tobias Shapinsky <tobiasshapinsky@gmail.com>
2018-03-10 13:47:05 -0500

    triangle rendering with colors on scaling canvas

--- hello_world/hello_world.c
+++ hello_world/hello_world.c
@@ -2,28 +2,53 @@
 #include <stdlib.h>
 #include <GL/glut.h>
 #include <GL/gl.h>
-void display(void)
-{
+void display(void) {
 
-    glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glColor3f(1.0,0.0,0.0);
-    glBegin(GL_POLYGON);
-    glVertex3f(0.0, 0.0, 0.0);
-    glVertex3f(0.25, 0.25, 0.0);
-    glVertex3f(0.5, 0.0, 0.0);
+    glBegin(GL_TRIANGLES);
+    glVertex3f(-2.0, 0.0, -5.0);
+    glColor3f(0.0,1.0,0.0);
+    glVertex3f(0.00, 2.0, -5.0);
+    glColor3f(0.0,0.0,1.0);
+    glVertex3f(2.0, 0.0, -5.0);
     glEnd();
     
-    glFlush();
+    glutSwapBuffers();
 }
 
-int main(int argc, char** argv)
-{
+void changeSize(int w, int h) {
+    
+	// Prevent a divide by zero, when window is too short
+	// (you cant make a window of zero width).
+	if(h == 0)
+		h = 1;
+	float ratio = 1.0* w / h;
+
+	// Use the Projection Matrix
+	glMatrixMode(GL_PROJECTION);
+
+        // Reset Matrix
+	glLoadIdentity();
+
+	// Set the viewport to be the entire window
+	glViewport(0, 0, w, h);
+
+	// Set the correct perspective.
+	gluPerspective(45,ratio,1,1000);
+
+	// Get Back to the Modelview
+	glMatrixMode(GL_MODELVIEW);
+}
+
+int main(int argc, char** argv) {
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE);	// Use single display buffer.
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
 	glutInitWindowSize(300, 300);
 	glutInitWindowPosition(100, 100);
 	glutCreateWindow("Hello world");
 	glutDisplayFunc(display);
+	glutReshapeFunc(changeSize);
 	glutMainLoop();
 	return 0;
 }
